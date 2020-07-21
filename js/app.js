d3ccfe477fbc546dcc3642216cdcfdf6535c5319
Uriel Alvarez <azaki14@gmail.com>
2020-07-21 16:04:15 -0500

    Funcionando toda la seccion de productos

--- js/app.js
+++ js/app.js
@@ -15,4 +15,18 @@ const articleSwiper = new Swiper("#swiperArticles", {
   slidesPerView: 1.5,
   centeredSlides: true,
   simulateTouch: true,
+  breakpoints: {
+    992: {
+      slidesPerView: 2.3,
+      centeredSlides: false,
+    },
+  },
 });
+
+window.addEventListener("resize", mediaQuerys);
+
+function mediaQuerys() {
+  if (window.matchMedia("(min-width: 992px)").matches) {
+    //cambiar el swiper
+  }
+}
